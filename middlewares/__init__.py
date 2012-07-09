41c1c1bad2c2ed5c13877ddf2a21c0a4c7048daa
Mohamed Attahri <mohamed.attahri@greendizer.com>
2012-07-09 17:40:28 +0100

    Main middleware now intercepts errors, and customizes the output in some cases.

--- middlewares/__init__.py
+++ middlewares/__init__.py
@@ -1,21 +1,28 @@
+from nuages.http import HttpException, HttpResponse
+
+
 class RequestHandlerMiddleware():
-    ''''''
-    
-    def process_request(self, request):
-        ''''''
-        pass
-    
-    
-    def process_view(self, request, view_func, view_args, view_kwargs):
-        ''''''
-        pass
-    
-    
-    def process_response(self, request, response):
-        ''''''
-        pass
-    
+#    def process_request(self, request):
+#        ''''''
+#        pass
+#    
+#    def process_view(self, request, view_func, view_args, view_kwargs):
+#        ''''''
+#        pass
+#    
+#    def process_response(self, request, response):
+#        ''''''
+#        return response
     
     def process_exception(self, request, exception):
         ''''''
-        pass
+        if not issubclass(exception.__class__, HttpException):
+            return
+        
+        node = exception.node
+        response = HttpResponse(request, status=exception.status)
+        
+        if response.status_code == 405: #Method Not Allowed
+            response['Allow'] = 'OPTIONS,' + ','.join(node.allows)
+            
+        return response
