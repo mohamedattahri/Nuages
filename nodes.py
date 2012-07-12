35853db53f032616b6112e4014e1adabade7b29f
Mohamed Attahri <mohamed.attahri@greendizer.com>
2012-07-12 10:19:49 +0100

    Basics of the serialization.
    Serialization in JSON, XML, and HTML
    Building absolute URIs
    Permissions tests and exceptions

--- nodes.py
+++ nodes.py
@@ -1,15 +1,21 @@
 # -*- coding: utf-8 -*-
 import inspect
+import urlparse
 from django.conf import settings
+from django.utils.importlib import import_module
 from django.core.urlresolvers import reverse, resolve
 from nuages.http import (HttpRequest, HttpResponse, InvalidRequestError,
-                         NotModifiedError, ETAG_WILDCARD)
+                         ForbiddenError, NotModifiedError, ETAG_WILDCARD,
+                         Range)
+from nuages.utils import serialization
 
 
 __all__ = ('Node', 'CollectionNode', 'ResourceNode')
 
 
-#settings.configure(DEFAULT_CONTENT_TYPE='application/json')
+API_ENDPOINT = urlparse.urlparse(settings.NUAGES_API_ENDPOINT
+                                 if hasattr(settings, 'NUAGES_API_ENDPOINT')
+                                 else '')
 
 
 class Node(object):
@@ -17,12 +23,14 @@ class Node(object):
     uri = None
     name = None
     parent = None
+    secure = False
     range_unit = ('resource', 'resources')
     max_limit = 200
-    outputs = ['application/json', 'application/xml']
+    outputs = ['application/json', 'application/xml',
+               'application/xhtml+xml', 'text/html', '*/*']
     allow = ['GET']
     __implicit_methods = ['OPTIONS', 'HEAD']
-    __parent_instance = None
+    _parent_instance = None
     
     def __new__(cls, *args, **kwargs):
         cls.outputs = [settings.DEFAULT_CONTENT_TYPE if c == '*/*' else c
@@ -30,10 +38,12 @@ class Node(object):
         return object.__new__(cls, *args, **kwargs)
     
     def __init__(self, request, *args, **kwargs):
-        values = locals()['args']
+        values = locals().get('args', tuple())
+        
+        self._try_cross()
         
         if self.parent:
-            self.__parent_instance = self.parent(request, **kwargs)
+            self._parent_instance = self.parent(request, **kwargs)
             
         self.__locals = {}
         members = filter(lambda x: x not in ['self', 'request'],
@@ -45,27 +55,57 @@ class Node(object):
         self.request = request
         
         if self.parent:
-            self.__parent_instance = self.parent(request, **kwargs)
+            self._parent_instance = self.parent(request, **kwargs)
         
         if request.method not in (self.allow + self.__implicit_methods):
             raise InvalidRequestError(self, 405) #Method not allowed
         
         if request.method != 'OPTIONS':
-            self.__matching_outputs = list(set(self.outputs) &
-                                           set(request.META.get('HTTP_ACCEPT',
+            self._matching_outputs = list(set(self.outputs) &
+                                          set(request.META.get('HTTP_ACCEPT',
                                                                 [])))
-            if not len(self.__matching_outputs):
+            if not len(self._matching_outputs):
                 raise InvalidRequestError(self, 406) #Not acceptable
-            
-    def build_uri(self):
-        ''''''
+    
+    def _can_cross(self):
+        return True
+        
+    def _can_read(self):
+        return False
+    
+    def _can_write(self):
+        return False
+    
+    def _try_cross(self):
+        if not self._can_cross():
+            raise ForbiddenError(self, 'Access to resource has been denied.')
+    
+    def _try_read(self):
+        if not self._can_read():
+            raise ForbiddenError(self, 'Access to resource has been denied.')    
+    
+    def _try_write(self):
+        if not self._can_write():
+            raise ForbiddenError(self, 'Access to resource has been denied.')
+    
+    def build_uri(self, absolute=True):
+        self._try_read() #You don't need the URI of a resource you can't see.
+        
         kwargs = self.__locals
         if self.parent:
-            parent_uri = self.__parent_instance.build_uri()
+            parent_uri = self._parent_instance.build_uri(absolute=False)
             kwargs.update(resolve(parent_uri)[2])
+
+        relative = reverse(self.__class__.get_view_name(), kwargs=kwargs)
+        if not absolute:
+            return relative
         
-        return reverse(self.__class__.get_view_name(),
-                       kwargs=kwargs)
+        return urlparse.urlunparse(('https' if self.secure else 'http',
+                                    API_ENDPOINT[1] or self.request.get_host(),
+                                    relative,
+                                    None,
+                                    None,
+                                    None))
 
     @property
     def etag(self):
@@ -79,7 +119,7 @@ class Node(object):
     def _get(self, *args):
         '''
         GET method documentation.
-        '''
+        '''       
         if ((self.request.META.get('HTTP_IF_MATCH', ETAG_WILDCARD) is not 
              self.etag) or
             self.request.META.get('HTTP_IF_NONE_MATCH') is self.etag):
@@ -90,8 +130,7 @@ class Node(object):
             raise NotModifiedError(self, 304) #Not modified
         
         response = HttpResponse(self, 200)
-        response.content = self.uri
-        response['Content-Type'] = self.__matching_outputs[0]
+        response['Content-Type'] = self._matching_outputs[0]
         response['Etag'] = self.etag
         response['Last-Modified'] = self.last_modified
         return response
@@ -119,7 +158,7 @@ class Node(object):
     
     def _delete(self, *args, **kwargs):
         '''
-        DELETES method documentation.
+        DELETE method documentation.
         '''
         raise NotImplementedError()
     
@@ -157,19 +196,45 @@ class Node(object):
     def to_dict(self):
         raise NotImplementedError()
     
-    def serialize(self, fields=None, mime_type='application/json'):
-        #TODO: Define serialization engines
-        serializables = self.to_dict()
-        map(serializables.pop, fields)
-        raise NotImplementedError() 
-    
+    def serialize(self, fields=[], mime_type=None):
+        self._try_read()
+        
+        mime_type = mime_type or self._matching_outputs[0]
+        data = dict([(k, v) for k,v in self.to_dict().items()
+                     if k not in fields])
+        for node in self.__class__.get_children_nodes():
+            try:
+                data.update({node.range_unit[1]:
+                             node(self.request, **self.__locals).build_uri()})
+            except ForbiddenError:
+                continue
+                
+        map(data.pop, fields)
+        
+        #TODO: implement a dynamic list of serialization engines
+        if mime_type in serialization.JSON_MIMETYPES:
+            return serialization.to_json(data)
+        elif mime_type in serialization.XML_MIMETYPES:
+            return serialization.to_xml(data)
+        elif mime_type in serialization.HTML_MIMETYPES:
+            return serialization.to_html(data)
+        else:
+            raise NotImplementedError()
+        
     def __repr__(self):
         try:
-            return self.serialize(mime_type=(self.__matching_outputs[0] 
-                                             if len(self.__matching_outputs)
+            return self.serialize(mime_type=(self._matching_outputs[0] 
+                                             if len(self._matching_outputs)
                                              else 'application/json'))
         except:
             return repr(super(Node, self))
+        
+    @classmethod
+    def get_children_nodes(cls):
+        root_urlconf = import_module(settings.ROOT_URLCONF)
+        return [url.callback.im_self for url in root_urlconf.urlpatterns
+                if url.callback.im_self.parent and
+                issubclass(url.callback.im_self.parent, cls) ]
         
     @classmethod
     def get_full_pattern(cls):
@@ -196,42 +261,37 @@ class Node(object):
 
 
 class CollectionNode(Node):
+    allow = ['GET', 'POST']
+    
     def fetch(self, query=None, offset=0, limit=200):
         if limit > self.max_limit:
             raise InvalidRequestError(self, 413) #Request Entity Too Large
         
         result = []
         if not len(result):
-            raise InvalidRequestError(self, 416) #Requested Range Not Satisfiable
+            raise InvalidRequestError(self, 416)    #Requested Range Not
+                                                    #Satisfiable
         
         return result
     
     def _get(self, query='', *args):
         response = super(CollectionNode, self)._get()
         
-        range = self.request.META.get('HTTP_RANGE')
-        offset, limit = (0, 200)
-        if range:
-            response.status = 206 #Partial
-            offset, limit = (range.offset, range.limit)
-        else:
+        request_range = self.request.META.get('HTTP_RANGE')
+        if not request_range:
+            request_range = Range(self.range_unit[1], 0, 200)
             response['Accept-Range'] = self.range_unit[1]
-        
-        response.content = self.fetch(query=filter, offset=offset,
-                                      limit=limit)
+                    
+        response.content = self.fetch(query=filter, offset=request_range.offset,
+                                      limit=request_range.limit)
         return response
-
-    def _post(self, *args, **kwargs):
-        pass
 
 
 class ResourceNode(Node):
+    allow = ['GET', 'PATCH']
     
     def _get(self, *args, **kwargs):
-        pass
-    
-    def _patch(self):
-        pass
-    
-    def _delete(self):
-        pass
+        '''Hello, this is a get function'''
+        response = super(ResourceNode, self)._get()
+        response.content = self.serialize(mime_type=self._matching_outputs[0])
+        return response
