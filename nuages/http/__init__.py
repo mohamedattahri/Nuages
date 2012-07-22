447968513e0bc1f5dfb08c81260e099de801c458
Mohamed Attahri <mohamed.attahri@greendizer.com>
2012-07-22 10:58:57 +0100

    Request validation has been moved to the middleware level, because several nodes can be loaded during a single request life span.

--- nuages/http/__init__.py
+++ nuages/http/__init__.py
@@ -1,9 +1,10 @@
 import re
 import itertools
 import collections
+import logging
 from datetime import datetime
 from django.conf import settings
-from django.http import HttpResponse as _HttpResponse
+from django.http import Http404, HttpResponse as _HttpResponse
 from django.core.handlers.wsgi import STATUS_CODE_TEXT
 
 
@@ -12,42 +13,18 @@ __all__ = ('HttpResponse', 'HttpResponse', 'Etag', 'Range', 'ContentRange',
            'HttpException')
 
 
-class HttpException(Exception):
-    def __init__(self, node, status=503,
-                 description=None):
-        self.__locals = locals()
-        map(lambda key: setattr(self, key, self.__locals[key]), self.__locals)
-        super(Exception, self).__init__(description or STATUS_CODE_TEXT[status])
-
-
-class InvalidRequestError(HttpException):
-    def __init__(self, node, status=400):
-        super(InvalidRequestError, self).__init__(node, status)
-
-
-class ForbiddenError(HttpException):
-    def __init__(self, node, description):
-        super(ForbiddenError, self).__init__(node, 403, description)        
-
-        
-class NotModifiedError(HttpException):
-    def __init__(self, node):
-        super(NotModifiedError, self).__init__(node, 304)
+logger = logging.getLogger(__name__)
         
         
 def datetime_to_timestamp(datetm):
-    '''
-    Returns the timestamp representation of a datetime instance.
-    '''
+    '''Returns the timestamp representation of a datetime instance.'''
     return float(datetm.strftime("%s") + 
                 ".%03d" % (datetm.time().microsecond / 1000))
     
     
 def parse_datetime(datestr):
-    '''
-    Turns a ISO8601 or RFC1123 date string representation to a Python
-    datetime instance.
-    '''
+    '''Turns a ISO8601 or RFC1123 date string representation to a Python
+    datetime instance.'''
     if 'T' in datestr:
         return datetime.strptime(datestr, '%Y-%m-%dT%T.%fZ')
     
@@ -58,9 +35,6 @@ def parse_datetime(datestr):
 class RequestMeta(collections.MutableMapping):
     '''Wrapper around the META dict of a Django HttpRequest instance'''
     def __init__(self, request_meta):
-        '''
-        @param request_meta:dict
-        '''
         self.store = request_meta
         
     def __getitem__(self, key):
@@ -79,6 +53,9 @@ class RequestMeta(collections.MutableMapping):
                             'HTTP_IF_UNMODIFIED_SINCE']:
                 return parse_datetime(value)
             
+            if header == 'Range':
+                return Range.parse(value)
+            
             if header == 'HTTP_ACCEPT':
                 items = filter(lambda x: not x.startswith('q='),
                                itertools.chain(*[i.split(',') 
@@ -87,7 +64,7 @@ class RequestMeta(collections.MutableMapping):
                         for i in items]
             
             return value
-        except(KeyError), e:
+        except(ValueError), e:
             raise InvalidRequestError(400, repr(e))
     
     def __setitem__(self, key, value):
@@ -112,42 +89,159 @@ class HttpRequest(object):
     and attributes.
     '''
     def __init__(self, base_request):
-        '''
-        @param base_request:django.http.HttpRequest
-        '''
         self._base_request = base_request
         self.META = RequestMeta(base_request.META)
         
     @property
     def method(self):
-        '''
-        Support for the X-HTTP-Method-Override header.
+        '''Support for the X-HTTP-Method-Override header.
         Returns the value of the header if set, falls back to the real HTTP
-        method if not.
-        '''
+        method if not.'''
         return self.META.get('X_HTTP_METHOD_OVERRIDE',
                              self._base_request.method)
         
     def __getattr__(self, name):
-        '''
-        Allows all the attributes of the base HttpRequest to be mirrored in
-        the wrapper, unless they've been overridden. 
-        '''
+        '''Allows all the attributes of the base HttpRequest to be mirrored in
+        the wrapper, unless they've been overridden. '''
         return getattr(self._base_request, name)       
     
     
 class HttpResponse(_HttpResponse):
+    '''A transparent wrapper around the Django HttpResponse class.
+    Currently not really useful, but this would save some time if the need
+    becomes clearer later.'''
     def __init__(self, request=None, *args, **kwargs):
         self.request = request
         super(HttpResponse, self).__init__(*args, **kwargs)
+        
+
+class HttpException(Exception, HttpResponse):
+    '''When raised, turns to an HTTP response detailing the error that
+    occurred. 
+    This class is both an Exception and an HttpResponse, which means it can
+    be "raised', or be the value returned by a view.'''
+    def __init__(self, node, status=503, description=''):
+        description = description or STATUS_CODE_TEXT[status]
+        Exception.__init__(self, description)
+        _HttpResponse.__init__(self, status=status,
+                               content=self.format_description())
+        self.__node = node
+        
+    @property
+    def node(self):
+        return self.__node
+    
+    def format_description(self):
+        return '<hello>%s</world>' % str(self)        
+
+        
+class NotModifiedError(HttpException):
+    '''"If the client has performed a conditional GET request and access is
+    allowed, but the document has not been modified, the server SHOULD respond
+    with this status code. The 304 response MUST NOT contain a message-body,
+    and thus is always terminated by the first empty line after the header
+    fields."'''
+    def __init__(self, node):
+        super(NotModifiedError, self).__init__(node, 304)
+        
+
+class InvalidRequestError(HttpException):
+    '''"The request could not be understood by the server due to malformed
+    syntax"'''
+    def __init__(self, node, status=400, description=''):
+        super(InvalidRequestError, self).__init__(node, status, description)
+        
+
+class Unauthorized(HttpException):
+    '''"The request requires user authentication.
+    The response MUST include a WWW-Authenticate header field (section 14.47)
+    containing a challenge applicable to the requested resource."
+    
+    (http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html#sec10.4.2)'''
+    def __init__(self, node):
+        super(Unauthorized, self).__init__(node, 401)
+        #TODO: Add a WWW-Authenticate header
+
+
+class ForbiddenError(HttpException):
+    '''The server understood the request, but is refusing to fulfill it.
+    Authorization will not help and the request SHOULD NOT be repeated.
+    If the request method was not HEAD and the server wishes to make public
+    why the request has not been fulfilled, it SHOULD describe the reason for
+    the refusal in the entity. If the server does not wish to make this
+    information available to the client, the status code 404 (Not Found)
+    can be used instead.'''
+    def __init__(self, node, description=None):
+        if not description:
+            raise Http404
+        super(ForbiddenError, self).__init__(node, 403, description)
+        
+        
+class MethodNotAllowedError(HttpException):
+    '''"The method specified in the Request-Line is not allowed for the
+    resource identified by the Request-URI. The response MUST include an
+    Allow header containing a list of valid methods for the requested
+    resource."'''
+    def __init__(self, node):
+        super(MethodNotAllowedError, self).__init__(node, 405)
+        self['Allow'] = ', '.join(node.allow)
+        
+        
+class NotAcceptableError(HttpException):
+    '''"The resource identified by the request is only capable of generating
+    response entities which have content characteristics not acceptable
+    according to the accept headers sent in the request.
+    
+    Unless it was a HEAD request, the response SHOULD include an entity
+    containing a list of available entity characteristics and location(s)
+    from which the user or user agent can choose the one most appropriate."'''
+    def __init__(self, node):
+        description = ', '.join(node.outputs)
+        super(NotAcceptableError, self).__init__(node, 406, description)
+        
+
+class ConflictError(HttpException):
+    '''"The request could not be completed due to a conflict with the current
+    state of the resource. This code is only allowed in situations where it is 
+    expected that the user might be able to resolve the conflict and resubmit
+    the request. The response body SHOULD include enough information for the
+    user to recognize the source of the conflict. Ideally, the response entity
+    would include enough information for the user or user agent to fix the
+    problem; however, that might not be possible and is not required."'''
+    def __init__(self, node, description=None):
+        super(ConflictError, self).__init__(node, 409, description)
+        
+        
+class PreconditionFailedError(HttpException):
+    '''"The precondition given in one or more of the request-header fields
+    evaluated to false when it was tested on the server."'''
+    def __init__(self, node, description=''):
+        super(PreconditionFailedError, self).__init__(node, 412, description)
+
+
+class UnsupportedMediaTypeError(HttpException):
+    '''"The server is refusing to service the request because the entity of
+    the request is in a format not supported by the requested resource for the
+    requested method."'''
+    def __init__(self, node, description=''):
+        super(UnsupportedMediaTypeError, self).__init__(node, 415, description)
+        
+        
+class RequestedRangeNotSatisfiableError(HttpException):
+    '''"A server SHOULD return a response with this status code if a request
+    included a Range request-header field (section 14.35), and none of the
+    range-specifier values in this field overlap the current extent of the
+    selected resource, and the request did not include an If-Range
+    request-header field. "'''
+    def __init__(self, node, description=''):
+        super(RequestedRangeNotSatisfiableError, self).__init__(node, 416,
+                                                                description)
 
 
 class Etag(object):
-    '''
-    The ETag response-header field provides the current value of the entity 
+    '''The ETag response-header field provides the current value of the entity 
     tag for the requested variant
-    (http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.19)
-    '''
+    (http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.19)'''
     def __init__(self, last_modified, id_):
         self.__locals = locals()
         setattr(self, 'timestamp', datetime_to_timestamp(last_modified))
@@ -175,16 +269,15 @@ ETAG_WILDCARD = Etag(datetime.fromtimestamp(0), '0')
 
 
 class Range(object):
-    '''
-    Parses the content of a Range header into a simple helper class.
+    '''Parses the content of a Range header into a simple helper class.
     
     HTTP retrieval requests using conditional or unconditional GET methods
     MAY request one or more sub-ranges of the entity, instead of the entire 
     entity.
-    (http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.35.2)
-    '''
+    (http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.35.2)'''
     def __init__(self, unit, offset, limit):
-        map(lambda key: setattr(self, key, locals()[key]), locals())
+        self.__locals = locals()
+        map(lambda key: setattr(self, key, self.__locals[key]), self.__locals)
     
     @classmethod
     def parse(cls, raw_header):
@@ -198,14 +291,12 @@ class Range(object):
 
 
 class ContentRange(object):
-    '''
-    Builds a valid Content-Range header representation as defined in the
+    '''Builds a valid Content-Range header representation as defined in the
     HTTP protocol.
     
     The Content-Range entity-header is sent with a partial entity-body to 
     specify where in the full entity-body the partial body should be applied.
-    (http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.16)
-    '''
+    (http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.16)'''
     def __init__(self, unit, first, last, total):
         self.__locals = locals()
         map(lambda key: setattr(self, key, self.__locals[key]), self.__locals)
--- nuages/nodes.py
+++ nuages/nodes.py
@@ -5,8 +5,10 @@ from django.conf import settings
 from django.utils.importlib import import_module
 from django.core.urlresolvers import reverse, resolve
 from nuages.http import (HttpRequest, HttpResponse, InvalidRequestError,
-                         ForbiddenError, NotModifiedError, ETAG_WILDCARD,
-                         Range)
+                         Range, ETAG_WILDCARD, PreconditionFailedError,
+                         RequestedRangeNotSatisfiableError, 
+                         ForbiddenError, NotModifiedError,)
+from nuages.utils import get_matching_mime_types
 from nuages.utils import serialization
 
 
@@ -19,15 +21,14 @@ API_ENDPOINT = urlparse.urlparse(settings.NUAGES_API_ENDPOINT
 
 
 class Node(object):
-    ''''''
     uri = None
     name = None
     parent = None
-    secure = False
-    range_unit = ('resource', 'resources')
+    secure = True
+    range_unit = None
     max_limit = 200
     outputs = ['application/json', 'application/xml',
-               'application/xhtml+xml', 'text/html', '*/*']
+               'application/xml+xhtml', 'text/html', '*/*']
     allow = ['GET']
     __implicit_methods = ['OPTIONS', 'HEAD']
     _parent_instance = None
@@ -35,60 +36,65 @@ class Node(object):
     def __new__(cls, *args, **kwargs):
         cls.outputs = [settings.DEFAULT_CONTENT_TYPE if c == '*/*' else c
                        for c in cls.outputs]
+        
+        cls.range_unit = cls.range_unit or cls.__name__ + 's' 
         return object.__new__(cls, *args, **kwargs)
     
     def __init__(self, request, *args, **kwargs):
-        values = locals().get('args', tuple())
+        '''Initializes a new instance of the Node class:
+         
+        - request: Current HttpRequest instance.
+        - *args, **kwargs are passed all the way to the top parent node.'''
+        self._try_cross()    
+            
+        self.request = request
+        members = filter(lambda x: x not in ['self', 'request'],
+                         inspect.getargspec(self.__init__)[0])
+        self.__locals = dict(zip(members, args))
+        map(lambda t: setattr(self, t[0], t[1]), self.__locals.items())
         
-        self._try_cross()
-        
+        #Create a new instance of the parent node class        
         if self.parent:
             self._parent_instance = self.parent(request, **kwargs)
             
-        self.__locals = {}
-        members = filter(lambda x: x not in ['self', 'request'],
-                         inspect.getargspec(self.__init__)[0])
-        for index, name in enumerate(members):
-            self.__locals.update({name: values[index]})
-            setattr(self, name, values[index])
-                
-        self.request = request
-        
-        if self.parent:
-            self._parent_instance = self.parent(request, **kwargs)
-        
-        if request.method not in (self.allow + self.__implicit_methods):
-            raise InvalidRequestError(self, 405) #Method not allowed
-        
-        if request.method != 'OPTIONS':
-            self._matching_outputs = list(set(self.outputs) & 
-                                          set(request.META.get('HTTP_ACCEPT',
-                                                                [])))
-            if not len(self._matching_outputs):
-                raise InvalidRequestError(self, 406) #Not acceptable
+        self._matching_outputs = get_matching_mime_types(request,
+                                                         self.__class__)
     
     def _can_cross(self):
+        '''Returns a boolean indicating whether the node can be crossed to
+        access a child node or not.'''
         return True
         
     def _can_read(self):
+        '''Returns a boolean indicating whether the resource of the node can
+        be serialized or not.'''
         return False
     
     def _can_write(self):
+        '''Returns a boolean indicating whether the resource of the node can
+        be modified or not'''
         return False
     
     def _try_cross(self):
+        '''Checks if the node is crossable, otherwise raises an exception.'''
         if not self._can_cross():
             raise ForbiddenError(self, 'Access to resource has been denied.')
     
     def _try_read(self):
+        '''Checks if the node is readable, otherwise raises an exception.'''
         if not self._can_read():
             raise ForbiddenError(self, 'Access to resource has been denied.')    
     
     def _try_write(self):
+        '''Checks if the node is modifiable, otherwise raises an exception.'''
         if not self._can_write():
             raise ForbiddenError(self, 'Access to resource has been denied.')
     
     def build_uri(self, absolute=True):
+        '''Dynamically builds the URI of the current node.
+        
+        - absolute: specifies whether to return an absolute URI, including the
+        API root configured in the settings.'''
         self._try_read() #You don't need the URI of a resource you can't see.
         
         kwargs = self.__locals
@@ -102,32 +108,28 @@ class Node(object):
         
         return urlparse.urlunparse(('https' if self.secure else 'http',
                                     API_ENDPOINT[1] or self.request.get_host(),
-                                    relative,
-                                    None,
-                                    None,
-                                    None))
+                                    relative, None, None, None))
 
     @property
     def etag(self):
-        #FIXME: Change this to different default value.
+        '''Gets the ETag of the current node.'''
         return ETAG_WILDCARD
     
     @property
     def last_modified(self):
+        '''Gets the date of the last modification of the current node.'''
         return self.etag.last_modified
     
-    def _get(self, *args):
-        '''
-        GET method documentation.
-        '''       
+    def _get(self, query=None, **kwargs):
+        '''GET method documentation.'''       
         if ((self.request.META.get('HTTP_IF_MATCH', ETAG_WILDCARD) is not 
              self.etag) or
             self.request.META.get('HTTP_IF_NONE_MATCH') is self.etag):
-            raise InvalidRequestError(self, 412) #Precondition Failed
+            raise PreconditionFailedError(self) #Precondition Failed
         
         if (self.request.META.get('HTTP_IF_MODIFIED_SINCE') < self.etag or
             self.request.META.get('HTTP_IF_UNMODIFIED_SINCE') > self.etag):
-            raise NotModifiedError(self, 304) #Not modified
+            raise NotModifiedError(self) #Not modified
         
         response = HttpResponse(self, 200)
         response['Content-Type'] = self._matching_outputs[0]
@@ -135,39 +137,30 @@ class Node(object):
         response['Last-Modified'] = self.last_modified
         return response
     
-    def _post(self, *args, **kwargs):
-        '''
-        POST method documentation.
-        '''
+    def _post(self):
+        '''POST method documentation.'''
         raise NotImplementedError()
     
-    def _patch(self, *args, **kwargs):
-        '''
-        PATCH method documentation.
-        '''
+    def _patch(self):
+        '''PATCH method documentation.'''
         if self.request['HTTP_IF_MATCH'] is self.etag:
             raise InvalidRequestError(self, 409) #Conflict
         
         return HttpResponse(self.request, self)
     
-    def _put(self, *args, **kwargs):
-        '''
-        PUT method documentation.
-        '''
+    def _put(self):
+        '''PUT method documentation.'''
         raise NotImplementedError()
     
-    def _delete(self, *args, **kwargs):
-        '''
-        DELETE method documentation.
-        '''
+    def _delete(self):
+        '''DELETE method documentation.'''
         raise NotImplementedError()
     
-    def _head(self, *args, **kwargs):
-        response = self._get(*args, **kwargs)
-        response.content = ''
-        return response
+    def _head(self):
+        '''HEAD method documentation.'''
+        return self._get()
     
-    def _options(self, *args, **kwargs):
+    def _options(self):
         '''
         The OPTIONS method represents a request for information about
         the communication options available on the request/response chain 
@@ -202,10 +195,11 @@ class Node(object):
         mime_type = mime_type or self._matching_outputs[0]
         data = dict([(k, v) for k, v in self.to_dict().items()
                      if k not in fields])
-        for node in self.__class__.get_children_nodes():
+        
+        for node_cls in self.__class__.get_children_nodes():
             try:
-                data.update({node.range_unit[1]:
-                             node(self.request, **self.__locals).build_uri()})
+                instance = node_cls(self.request, **self.__locals)
+                data.update({node_cls.__name__ : instance.build_uri()})
             except ForbiddenError:
                 continue
                 
@@ -220,14 +214,7 @@ class Node(object):
             return serialization.to_html(data)
         else:
             raise NotImplementedError()
-        
-    def __repr__(self):
-        try:
-            return self.serialize(mime_type=(self._matching_outputs[0] 
-                                             if len(self._matching_outputs)
-                                             else 'application/json'))
-        except:
-            return repr(super(Node, self))
+            
         
     @classmethod
     def get_children_nodes(cls):
@@ -254,24 +241,22 @@ class Node(object):
         return cls.__module__.replace('.', '_') + '_' + cls.__name__
         
     @classmethod
-    def process(cls, request, *args, **kwargs):
+    def process(cls, request, **kwargs):
         instance = cls(HttpRequest(request), **kwargs)
         method_func = getattr(instance, '_' + request.method.lower())
         return method_func()
 
 
 class CollectionNode(Node):
-    allow = ['GET', 'POST']
+    allow = ['GET']
     
     def fetch(self, query=None, offset=0, limit=200):
-        if limit > self.max_limit:
-            raise InvalidRequestError(self, 413) #Request Entity Too Large
+        if self.max_limit and limit > self.max_limit:
+            raise InvalidRequestError(self, 400,
+                                      'Response is too large. Retry with a ' \
+                                      '"Range" request-header.')
         
         result = []
-        if not len(result):
-            raise InvalidRequestError(self, 416)    #Requested Range Not
-                                                    #Satisfiable
-        
         return result
     
     def _get(self, query='', *args):
@@ -279,19 +264,26 @@ class CollectionNode(Node):
         
         request_range = self.request.META.get('HTTP_RANGE')
         if not request_range:
-            request_range = Range(self.range_unit[1], 0, 200)
-            response['Accept-Range'] = self.range_unit[1]
-                    
-        response.content = self.fetch(query=filter, offset=request_range.offset,
-                                      limit=request_range.limit)
+            used_paging = True
+            request_range = Range(self.range_unit, 0, self.max_limit)
+            response['Accept-Range'] = self.range_unit
+        
+        child_nodes = self.fetch(query=filter, offset=request_range.offset,
+                                 limit=request_range.limit)
+        
+        if not len(child_nodes):
+            if used_paging:
+                raise RequestedRangeNotSatisfiableError(self)
+            
+            response.status = 204
+        
         return response
 
 
 class ResourceNode(Node):
-    allow = ['GET', 'PATCH']
+    allow = ['GET']
     
     def _get(self, *args, **kwargs):
-        '''Hello, this is a get function'''
         response = super(ResourceNode, self)._get()
         response.content = self.serialize(mime_type=self._matching_outputs[0])
         return response
