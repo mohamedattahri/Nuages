ac8a4c78958a31fdfa809894717a0f19b8527f50
Mohamed Attahri <mohamed.attahri@greendizer.com>
2012-09-25 15:48:00 +0100

    Added custom attributes, among which a boolean that checks if the data received doesn't contain unexpected parameters.

--- nuages/forms.py
+++ nuages/forms.py
@@ -1,9 +1,25 @@
 # -*- coding: utf-8 -*-
-from django.forms import Form
+from django.forms import Form, fields
+from django.forms import ValidationError
 from django.utils.encoding import force_unicode
 
 
+class UnexpectedFieldsError(Exception):
+    def __init__(self, field_names):
+        super(UnexpectedFieldsError, self).__init__('Unexpected field(s): %s' %
+                                                    ', '.join(field_names))
+
+
 class Form(Form):
+    def __init__(self, data=None, strict=True, node=None, *args, **kwargs):
+        super(Form, self).__init__(data=data, *args, **kwargs)
+        self.node = node
+        if strict:
+            unexpected_fields = [key for key in data
+                                 if key not in self.fields]
+            if len(unexpected_fields):
+                raise UnexpectedFieldsError(unexpected_fields)
+    
     def errors_as_text(self):
         lines = []
         for key, messages in self.errors.items():
