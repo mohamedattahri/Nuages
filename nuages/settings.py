3d13b58c9fa43ed9be37154f0a11a94bb6faa026
Mohamed Attahri <mohamed@attahri.com>
2014-07-01 17:03:41 +0000

    Middleware fails to be loaded when added dynamically.

--- nuages/settings.py
+++ nuages/settings.py
@@ -3,11 +3,6 @@ import os
 from django.conf import settings
 
 
-settings.MIDDLEWARE_CLASSES = getattr(settings, 'MIDDLEWARE_CLASSES', ()) + (
-    'nuages.middlewares.RequestHandlerMiddleware',
-)
-
-
 settings.TEMPLATE_DIRS = getattr(settings, 'TEMPLATE_DIRS', []) + [
     os.path.join(os.path.dirname(os.path.dirname(__file__)), 'templates'),
 ]
